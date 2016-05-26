e3867c03d9a6206bb9586390ea8fbb33708946d0
Fahadshawal <bsef14m558@pucit.edu.pk>
2016-05-26 21:49:01 +0500

    chnag in port number only!

--- Client.py
+++ Client.py
@@ -3,7 +3,7 @@ from threading import Thread
 import sys
 
 HOST = 'localhost'
-PORT = 5189
+PORT = 5160
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
 
@@ -11,14 +11,16 @@ Sock = socket(AF_INET, SOCK_STREAM)
 Sock.connect(ADDR)
 
 def recv():
-    while True:
-        data = Sock.recv(BUFSIZE)
-        if not data: sys.exit(0)
-        print data
+	while True:
+		data = Sock.recv(BUFSIZE)
+		if not data: sys.exit(0)
+		print data
 
 Thread(target=recv).start()
+data = raw_input('Please Enter Your User Name : ')
+Sock.send(data)
 while True:
-    data = raw_input('> ')
+    data = raw_input('Me: ')
     Sock.send(data)
 
 Sock.close()
