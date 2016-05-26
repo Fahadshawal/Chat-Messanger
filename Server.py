0af3e217e30a5576dbaef0f025364bf2244e492c
Fahadshawal <bsef14m558@pucit.edu.pk>
2016-05-26 21:47:45 +0500

    showing of user adder

--- Server.py
+++ Server.py
@@ -7,7 +7,7 @@ import sys
 from thread import *
  
 HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 5189 # Arbitrary non-privileged port
+PORT = 5160# Arbitrary non-privileged port
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
@@ -28,33 +28,61 @@ print 'Socket now listening'
 #Function for handling connections. This will be used to create threads
 def clientthread(conn):
     #Sending message to connected client
-    conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
-     
+	#loop = len(arr)
+	#conn.send(str(loop)) #send only takes string 
+	
     #infinite loop so that function do not terminate and thread do not end.
-    while True:
+	while 1:
          
         #Receiving from client
-        data = conn.recv(1024)
-        if conn == arr[0]:
-        	conn = arr[1]
-        	conn.send(data)
-        	conn = arr[0]
-	elif conn == arr[1]:
-		conn = arr[0]
-		conn.send(data)
-		conn = arr[1]
-        if not data: 
-            break
-     
+		data = conn.recv(1024)
+		if conn == arr[0]['con']:
+			conn = arr[1]['con']
+			conn.send(data)
+			conn = arr[0]['con']
+		elif conn == arr[1]['con']:
+			conn = arr[0]['con']
+			conn.send(data)
+			conn = arr[1]['con']
     #came out of loop
-    conn.close()
+	conn.close()
+
+#-------------------------------------------------------
+#This Function will show the list of all connected users
+def ShowAllUser(conn):
+	conn.send("\n---------------Connected User With The Serve Are---------------\n")
+	loop = len(arr)
+	while loop:
+		loop -= 1
+		conn.send(arr[loop]['nam']+"\n")
+	conn.send("---------------------------------------------------------------\n")
+	return
+	
+#------------------------------------------------------
+#This Function WIll Show to All User About New Connection
+def ShowToAll(temp):
+	loop = len(arr)
+	while loop:
+		loop -= 1
+		arr[loop]['con'].send("--------------------------------------")
+		arr[loop]['con'].send("\n" + temp['nam'] + " is Now Connected\n")
+		arr[loop]['con'].send("--------------------------------------")
+	return
+
 arr =[] # this is to keep track of users
 i = 0
 #now keep talking with the client
 while 1:
     #wait to accept a connection - blocking call
     conn, addr = s.accept()
-    arr.append(conn)
+    temp = {}
+    temp['con'] = conn
+    temp['adr'] = addr
+    temp['nam'] = conn.recv(200)
+    ShowToAll(temp)
+    arr += [temp]
+    ShowAllUser(conn)
+    #print arr
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
      
     #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
